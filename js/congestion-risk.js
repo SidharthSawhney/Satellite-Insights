956a5c1e601670ddc1ad9ff6bdaaa4b180272ddb
amvipat <amvi.patel@mail.utoronto.ca>
2025-10-24 23:23:14 -0400

    Implemented the condensed view for the congestion graph

--- js/congestion-risk.js
+++ js/congestion-risk.js
@@ -4,13 +4,17 @@
 
 class Congestion {
     /**
-     * @param {string} hostId            Container element id
-     * @param {Array<Object>} data       Rows with keys: 'perigee_(km)', 'apogee_(km)', 'inclination_(degrees)', 'class_of_orbit'
+     * @param {string} hostId                   Container element id
+     * @param {Array<Object>} data              Rows with keys: 'perigee_(km)', 'apogee_(km)', 'inclination_(degrees)', 'class_of_orbit'
+     * @param {Array<Object>} dataCondensed     Data condensed into 4 categories
+     * @param {integer>} state                  State of the visualization
+
      */
-    constructor(hostId, data) {
+    constructor(hostId, data, condensed) {
         this.host = d3.select('#' + hostId);
         this.node = this.host.node();
         this.data = data || [];
+        this.dataCondensed = condensed;
 
         // dimensions
         const box = this.node.getBoundingClientRect();
@@ -20,7 +24,10 @@ class Congestion {
         // color scale for orbit classes
         this.colorScale = d3.scaleOrdinal()
             .domain(["LEO", "MEO", "GEO", "Elliptical"])
-            .range(['#d2b0f6ff', '#9ee7f6ff', '#c2e98cff', '#f2d396ff']);
+            .range(['#22ff00ff', '#05d4fdff', '#f80cccff', '#fbff2cff']);
+
+        //sets current state to 0
+        this.state = 0;
 
         // responsive
         if (window.ResizeObserver) {
@@ -33,7 +40,7 @@ class Congestion {
     initVis() {
         const vis = this;
 
-        // === Create the main SVG ===
+        // Create the main SVG
         vis.svg = vis.host.append('svg')
             .attr('class', 'congestion-svg')
             .attr('width', vis.width)
@@ -41,7 +48,7 @@ class Congestion {
 
 
 
-        // === Globe Projection ===
+        // Globe Projection
         vis.projection = d3.geoOrthographic()
             .scale(vis.height / 7)
             .translate([vis.width / 2, vis.height / 2])
@@ -50,14 +57,14 @@ class Congestion {
         vis.path = d3.geoPath(vis.projection);
 
 
-        // === Background Sphere ===
+        // Background Sphere
         vis.svg.append("circle")
             .attr("cx", vis.width / 2)
             .attr("cy", vis.height / 2)
             .attr("r", vis.projection.scale())
             .attr("fill", "#1a69b9ff");
 
-        // === Load World Map ===
+        // Load World Map 
         d3.json("https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json").then(worldData => {
             const countries = topojson.feature(worldData, worldData.objects.countries);
 
@@ -78,17 +85,9 @@ class Congestion {
                 vis.land.attr("d", vis.path);
             });
         });
-        // === Orbit Layer (empty for now) ===
+        // Orbit Layer (empty for now) 
         vis.orbitLayer = vis.svg.append('g')
             .attr('class', 'orbits');
-
-
-        // === Call updateVis to draw orbits ===
-        vis.updateVis();
-    }
-
-    updateVis() {
-        const vis = this;
 
         // scale distances to pixels based on max apogee
         const maxApogee = d3.max(vis.data, d => +d['apogee_(km)'] || 0) || 1;
@@ -97,7 +96,141 @@ class Congestion {
         vis.scale = d3.scaleLinear()
             .domain([0, maxApogee])
             .range([0, maxRadius]);
+        
+        
+        // Create scales for condensed view - where stroke width indicates satellite value
+        vis.cScale = d3.scaleLinear()
+                        .domain(d3.extent(vis.dataCondensed, d => d.count))
+                        .range([4,10]);
+        
+        const earthRadius = vis.projection.scale();
+        vis.orbitScale = d3.scaleOrdinal()
+                            .domain(["LEO", "MEO", "GEO", "Elliptical"])
+                            .range([earthRadius*1.2,earthRadius*1.4,earthRadius*1.6,earthRadius*1.8]);
+        
+        // Sort the data
+        vis.dataCondensed = vis.dataCondensed.sort((a, b) => {
+                // Sort by orbitScale value, descending (largest radius first)
+                return vis.orbitScale(b.orbit_class) - vis.orbitScale(a.orbit_class);
+            });
 
+        
+        // Show the color legend
+        vis.legend = vis.svg.append("g")
+                        .attr("class", "legend");
+        vis.legend.selectAll("rect")
+                    .data(vis.colorScale.domain())
+                    .enter()
+                    .append("rect")
+                    .attr("x",0)
+                    .attr("y", (d,i)=> i*25)
+                    .attr("width", 20)
+                    .attr("height", 20)
+                    .style("fill", d=> this.colorScale(d));
+
+        vis.legend.selectAll("text")
+                    .data(vis.colorScale.domain())
+                    .enter()
+                    .append("text")
+                    .attr("class", "legend_text")
+                    .attr("x", 30)
+                    .attr("y", (d,i) => i*25 + 15)
+                    .text(d=>d);
+        
+        // Add a button to change states
+        vis.button = vis.svg.append("g")
+                            .attr("class", "svg-button")
+                            .attr("transform", `translate(${vis.width/2-80}, 0)`)
+                            .style("cursor", "pointer")
+                             .on("click",function(){
+                                // Get the text of the button
+                                let txt = d3.select(this).select(".buttonText").text();
+                                
+                                // Depending on current text, change state
+                                if (txt === "Full View"){
+                                    d3.select(this).select(".buttonText").text("Simplify");
+                                    vis.state = 0;
+                                }
+                                else{
+                                     d3.select(this).select(".buttonText").text("Full View");
+                                     vis.state = 1;
+                                }
+
+                                vis.removeOrbits();
+                            
+
+                             });
+        vis.button.append("rect")
+                .attr("class", "buttonBox")
+                .attr("width", 140)
+                .attr("height", 40)
+                ;
+
+        vis.button.append("text")
+                .attr("class", "buttonText")
+                .attr("x", 20)
+                .attr("y", 25)
+                .text("Simplify Me");
+
+        
+
+        // Call updateVis to draw orbits
+        vis.updateVis();
+    }
+
+    // Transition out the orbits
+    removeOrbits(){
+         const vis = this
+           const orbits = vis.svg.selectAll(".orbit");
+           let remaining = orbits.size();
+
+           if (remaining ===0){
+            vis.updateVis();
+            return;
+           }
+
+           orbits.transition()
+           .duration(500)
+           .style("opacity", 0)
+           .on("end", ()=>{
+                remaining--;
+                if (remaining ===0 ) {
+                    vis.updateVis();}
+            })
+            .remove();
+
+        
+
+    }
+
+    updateVis() {
+        const vis = this;
+        if(vis.state === 1){
+            const orbits = vis.orbitLayer.selectAll('.orbit')
+                            .data(vis.dataCondensed);
+            
+            // Enter and merge
+            orbits.enter().append('circle')
+                            .merge(orbits)
+                            .attr('cx', vis.width/2)
+                            .attr('cy', vis.height/2)
+                            .attr('r',(d,i)=>{
+                                return vis.orbitScale(d.orbit_class);
+                            })
+                            .attr('class', 'orbit')
+                            .attr('fill', 'none')
+                            .attr('stroke', d => vis.colorScale(d.orbit_class))
+                            .attr('stroke-width', d=>{
+                                return vis.cScale(d.count);
+                            });
+
+             //Remove unused orbits   
+             orbits.exit().remove();
+
+
+        }
+
+        if (vis.state === 0){
         // DATA JOIN
         const orbits = vis.orbitLayer.selectAll('.orbit')
             .data(vis.data, d => d.norad_number || d['current_official_name_of_satellite'] || Math.random());
@@ -130,6 +263,12 @@ class Congestion {
 
         // EXIT
         orbits.exit().remove();
+
+        }
+        else{
+
+        }
+
     }
 
     resize() {
@@ -150,8 +289,6 @@ class Congestion {
 
 
 
-        
-        vis.updateVis();
-
+    
     }
 }
